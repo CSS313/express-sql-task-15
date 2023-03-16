cb86d21e687e3a39d1e06635f3ef9a7841b23be7
Chetan Singh <singh.chetan.313@gmail.com>
2023-03-16 23:25:18 +0530

    express-sql-task-15

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'priyatan@3009'
+});
+
+module.exports = pool.promise();
